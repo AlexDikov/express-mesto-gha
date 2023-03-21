194c7903a1b7176bfa186784d3efe0f4afc13742
Alex Dikov <dikov_alex@mail.ru>
2023-03-21 23:55:01 +0300

    дальше не знаю как

--- models/card.js
+++ models/card.js
@@ -10,6 +10,8 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
+    // eslint-disable-next-line no-useless-escape
+    pattern: '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/',
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
+const { default: isEmail } = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,11 +18,14 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://gazetavolgodonsk.ru/wp-content/uploads/2020/04/ken-rodrigo-alves-hochet-smenit-pol-i-stat-barbi7.jpg',
+    // eslint-disable-next-line no-useless-escape
+    pattern: '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/',
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    validate: [isEmail, 'неккоректный email'],
   },
   password: {
     type: String,
