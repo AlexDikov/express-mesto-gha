34e01aa0bc856508ae99db072cd8f8cd10586d82
Alex Dikov <dikov_alex@mail.ru>
2023-03-26 14:51:21 +0300

    исправляю ошибки теста

--- controllers/users.js
+++ controllers/users.js
@@ -43,7 +43,13 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => { res.send({ data: user }); })
+    .then((user) => {
+      res.send({
+        data: {
+          name: user.name, about: user.about, avatar: user.avatar, email: user.email,
+        },
+      });
+    })
     .catch((err) => {
       if (err.code === 11000) {
         throw new ConflictError('Пользователь с таким email уже зарегистрирован');
