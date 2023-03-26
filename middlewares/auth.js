1513bede36ec1f3b55d643abbf2b771745465305
Alex Dikov <dikov_alex@mail.ru>
2023-03-26 17:16:15 +0300

    исправил ошибки теста

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const BadRequestError = require('../errors/BadRequestError');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.cookies;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Пользователь не авторизован');
+    throw new BadRequestError('Неверное что-то');
   }
 
   const token = authorization.replace('Bearer ', '');
--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
-    default: 'https://gazetavolgodonsk.ru/wp-content/uploads/2020/04/ken-rodrigo-alves-hochet-smenit-pol-i-stat-barbi7.jpg',
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     // eslint-disable-next-line no-useless-escape
     pattern: '/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/',
   },
